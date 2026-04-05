44b638c565bb9f740068de2d749445f20eede33b
Justin Cabral <70143234+justinhcabral@users.noreply.github.com>
2026-04-05 19:40:17 +0800

    Logic for focus-mode extension

--- focus-mode/background.js
+++ focus-mode/background.js
@@ -3,3 +3,28 @@ chrome.runtime.onInstalled.addListener(() => {
     text: "OFF",
   });
 });
+
+/*
+Step by step logic for implementation of tracking the state of the current tab
+
+  After the user clicks the extension action, the extension will check if the URL matches a documentation page. Next, it will check the state of the current tab and set the next state. 
+*/
+
+// 1. Define the URLs tro track
+const extensions = "https://developer.chrome.com/docs/extensions";
+const webstore = "https://developer.chrome.com/docs/webstore";
+
+// 2. Listen for the extension action click
+chrome.action.onClicked.addListener(async (tab) => {
+  // 3. Check if the tab's URL matches the target URLs
+  if (tab.url?.startsWith(extensions) || tab.url?.startsWith(webstore)) {
+    // 4. Retrieve the current badge status
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const nextState = prevState === "ON" ? "OFF" : "ON";
+    // 5. Determine the next state
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState,
+    });
+  }
+});
